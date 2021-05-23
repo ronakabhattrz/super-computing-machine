bc68a06d142936996987cf120fc7f9c562c10e0d
RONAK BHATT <ronakabhattrz@gmail.com>
2021-05-23 17:41:34 +0530

    add responsve css

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,15 +6,15 @@ export default function App() {
   return (
     <div className="App">
       <Container maxWidth="md">
-        <img src="/largest-vaccine-banner.jpeg" alt="banner" style={{width: 800,marginTop: 5}} />
-        <img src="/banner2.png" alt="banner2" style={{width: 800,marginTop: 5}} />
+        <img src="/largest-vaccine-banner.jpeg" alt="banner" />
+        <img src="/banner2.png" alt="banner2" />
         <Button variant="contained" color="primary" href="https://prod-cdn.preprod.co-vin.in/assets/pdf/User_Guide_Citizen%20registration_18+.pdf" target="_blank">
           Know how to book your appointment
         </Button>
         <Button variant="contained" color="secondary" href="https://prod-cdn.preprod.co-vin.in/assets/pdf/Dos_and_Donts_for_Citizens.pdf" target="_blank">
           Dos and Don'ts
         </Button>
-        <img src="/banner3.png" alt="banner3" style={{width: 800,marginTop: 5}} />
+        <img src="/banner3.png" alt="banner3" />
       </Container>
     </div>
   );
