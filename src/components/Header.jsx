301463edc71929756c446aaa130d9dda2d93abb3
RONAK BHATT <ronakabhattrz@gmail.com>
2021-05-23 17:47:54 +0530

    add css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles({
   navbarDisplayFlex: {
     display: `flex`,
-    justifyContent: `space-between`
+    justifyContent: `space-between`,
   },
   navDisplayFlex: {
     display: `flex`,
