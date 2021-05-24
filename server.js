681feb66574f78701eefa8668544e6d583d0bc62
Ronak Bhatt <ronakabhattrz@gmail.com>
2021-05-24 11:02:28 +0530

    changes

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
 
-// set the home page route
+// set the home page route 
 app.get('/', function(req, res) {
 
     // ejs render automatically looks in the views folder
